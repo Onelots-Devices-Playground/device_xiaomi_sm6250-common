791379429f05b5ffd5a644206808f85548741731
Onelots <onelots@onelots.fr>
2025-03-17 23:39:30 +0100

    sm6250-common: Initialize EvolutionX
    Signed-off-by: Onelots <onelots@onelots.fr>

--- atoll.mk
+++ atoll.mk
@@ -164,6 +164,9 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+DEVICE_PACKAGE_OVERLAYS += \
+     $(LOCAL_PATH)/overlay-evolution
+
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     SM6250Frameworks \
